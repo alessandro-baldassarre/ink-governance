92ec73375739fe42e1600ab57662098f14e0c065
Alessandro Baldassarre <alessandro.baldassarre.dev@gmail.com>
2023-02-21 16:51:11 +0100

    Add counting and voting errors

---
+++ crate/src/traits/errors/counting.rs
@@ -0,0 +1,22 @@
+use openbrush::traits::String;
+
+use super::GovernorError;
+
+#[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum CountingError {
+    /// Custom error type for cases if writer of traits added own restrictions
+    Custom(String),
+    /// Error from Governor
+    GovernorError(GovernorError),
+    /// Returns when an account already cast a vote of that proposal
+    VoteAlreadyCast,
+    /// Returns when cast an invalid vote type
+    InvalidVoteType,
+}
+
+impl From<GovernorError> for CountingError {
+    fn from(_governor: GovernorError) -> Self {
+        CountingError::Custom(String::from("G::Governor Error"))
+    }
+}
--- crate/src/traits/errors/mod.rs
+++ crate/src/traits/errors/mod.rs
@@ -1,7 +1,10 @@
+mod counting;
 mod counting_simple;
 mod governor;
+mod voting;
 mod voting_group;
 
+pub use counting::CountingError;
 pub use counting_simple::CountingSimpleError;
 pub use governor::GovernorError;
 pub use voting_group::VotingGroupError;
