347df4cd723afd49ec7d22c210ef9ec1aa6d9396
Alessandro Baldassarre <alessandro.baldassarre.dev@gmail.com>
2023-03-10 13:38:21 +0100

    cargo fmt --all

--- crate/src/traits/governance/mod.rs
+++ crate/src/traits/governance/mod.rs
@@ -4,10 +4,10 @@ pub use governor::*;
 
 /// Traits implementation of modules to use combined with governor.
 pub mod modules {
-    pub mod counting_simple;
-    pub mod voting_group;
-    pub mod voter;
     pub mod counter;
+    pub mod counting_simple;
+    pub mod voter;
+    pub mod voting_group;
 }
 
 /// Traits implementation of extensions of governor base contracts.
